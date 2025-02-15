fb9f2c647817ef30c4ce07b1ac83bf38c155ded2
belyaikin <dmitriybelyaikin@gmail.com>
2025-02-15 12:06:21 +0500

    fixes

--- src/main/java/me/belyaikin/minimoodle/MiniMoodleApplication.java
+++ src/main/java/me/belyaikin/minimoodle/MiniMoodleApplication.java
@@ -1,10 +1,13 @@
 package me.belyaikin.minimoodle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniMoodleApplication {
+	public static final Logger LOGGER = LoggerFactory.getLogger(MiniMoodleApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniMoodleApplication.class, args);
